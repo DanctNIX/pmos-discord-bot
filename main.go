d46c4f8e85e67cb88a31651c835b0851cd4b0f1a
Danct12 <danct12@disroot.org>
2020-04-13 11:54:40 +0700

    Change copyright holder
    Signed-off-by: Danct12 <danct12@disroot.org>

--- main.go
+++ main.go
@@ -3,7 +3,7 @@
 //
 // Copyright (C) 2017 Tulir Asokan
 // Copyright (C) 2018-2019 Luca Weiss
-// Copyright (C) 2020 Danct12
+// Copyright (C) 2020 DanctNIX Community
 //
 // This program is free software: you can redistribute it and/or modify
 // it under the terms of the GNU General Public License as published by
